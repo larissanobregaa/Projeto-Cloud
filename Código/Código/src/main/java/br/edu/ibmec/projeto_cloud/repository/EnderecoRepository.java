c7e18ae7edb62b2ddea0786534fdc76fec9d0dc8
Larissa Nobrega <larissa.amn@hotmail.com>
2024-10-21 10:11:08 -0300

    Removendo a pasta Código

--- Código/Código/src/main/java/br/edu/ibmec/projeto_cloud/repository/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ibmec.projeto_cloud.repository;
-
-import br.edu.ibmec.projeto_cloud.model.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
-}
